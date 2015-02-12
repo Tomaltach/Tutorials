ed3527a600e5713a984a375effd96a6b373084f3
Tomaltach <thomas.donegan@mycit.ie>
2015-02-12 14:00:58 +0000

    Call another BATCH file from another. Trying to add more to the searching directories to search every mounted drive.

--- Batch/My Own Code/utilities.bat
+++ Batch/My Own Code/utilities.bat
@@ -1,11 +1,11 @@
 @echo off
 
-title List Directories
+title Utilities
 :menu
 cls
 echo #########################################
 echo #                                       #
-echo # Welcome to the console                #
+echo # Welcome to the Utilities console      #
 echo #                                       #
 echo #########################################
 echo #                                       #
@@ -13,6 +13,7 @@ echo # 1. List files in current directory.   #
 echo # 2. List files in chosen directory.    #
 echo # 3. List files with type.              #
 echo # 4. List files with name.              #
+echo # 5. Test ipconfig.                     #
 echo #                                       #
 echo # q. Quit                               #
 echo #                                       #
@@ -24,10 +25,13 @@ if %udefine%==1 goto f1
 if %udefine%==2 goto f2
 if %udefine%==3 goto f3
 if %udefine%==4 goto f4
+if %udefine%==5 goto f5
 if %udefine%==q goto quit
 :f1
 cls
-::print the current directory
+::#############################
+::#print the current directory#
+::#############################
 echo %cd%
 echo.
 ::redirect the directory list to echo to print to screen
@@ -37,6 +41,9 @@ echo.
 pause
 goto menu
 :f2
+::################################################################
+::#create a file with all of the contents in the chosen directory#
+::################################################################
 cls
 set /p directory=Enter the directory you want to view: 
 echo.
@@ -55,6 +62,9 @@ pause
 if %choice%==n goto menu
 goto menu
 :f3
+::####################################################
+::#search every directory for the chosen type of file#
+::####################################################
 ::Build YYYYMMDD_hhmmss format date/time stamp for new file name
 set stamp=%DATE:~-4%%DATE:~-10,2%%DATE:~-7,2%_%TIME:~0,8%
 set stamp=%stamp::=%
@@ -65,13 +75,29 @@ echo | dir /s c:\*.%type% > "All %type% files INFO - %stamp%.txt" | more
 echo | dir /b /s c:\*.%type% > "All %type% files NAME - %stamp%.txt" | more
 goto menu
 :f4
+::######################################################
+::#search every directory for the chosen name of a file#
+::######################################################
 ::Build YYYYMMDD_hhmmss format date/time stamp for new file name
 set stamp=%DATE:~-4%%DATE:~-10,2%%DATE:~-7,2%_%TIME:~0,8%
 set stamp=%stamp::=%
 set stamp=%stamp: =0%
 ::get file name to search
 set /p name=Search name: 
-echo | dir /b /s c:\%name%.* > "All %name% files - %stamp%.txt" | more
+::loop to check every drive
+for %%d in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+if not exist %%d:\nul (
+echo %%d:\ drive is not connected
+)
+pause
+if exist %%:\nul (
+echo checking %%d:\ now
+echo | dir /b /s %%d:\%name%.* >> "All %name% files - %stamp%.txt" | more
+)
+)
+goto menu
+:f5
+call ipconfig.bat
 goto menu
 :quit
 echo.
